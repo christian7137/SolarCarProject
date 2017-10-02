8c3c75ba7e06b53248c2f45e4a7ca734abd4afc5
John Roland <johnroland@utexas.edu>
2017-10-02 15:30:17 -0500

    debugged and working. threads can now update the shared sensor data. a seperate payload thread is creating can messages. currently it's printing this information out. Next, consider adding mutex. Next, Create FIFO queue and canager thread.

--- new_acquisition/can_structs.hh
+++ new_acquisition/can_structs.hh
@@ -8,24 +8,8 @@
 #ifndef CAN_MSG_HH
 #define CAN_MSG_HH
 
-#pragma push()
+#pragma push
 #pragma pack(1)
-
-/* CAN PAYLOAD STRUCT */
-#define MAX_CAN_MSG_LEN 8
-typedef struct{
-	union{
-		char type:4;
-		char extraPayload:4;
-		char data;
-	}byte0;
-    union{
-        unsigned char pData[MAX_CAN_MSG_LEN - 1 ];
-        canPktType1 data1;
-        canPktType2 data2;
-    }data;
-}CAN_MSG;
-
 
 /* USER CAN PAYLOAD PACKING TYPES */
 typedef struct{
@@ -50,6 +34,17 @@ typedef struct{
 #define PKT_SIZE_3 sizeof(int)
 #define PKT_TYPE_3 3
 
-#pragma pop()
+/* CAN PAYLOAD STRUCT */
+#define MAX_CAN_MSG_LEN 8
+typedef struct{
+    char type;
+    union{
+        unsigned char pData[MAX_CAN_MSG_LEN - 1 ];
+        canPktType1 type1;
+        canPktType2 type2;
+        canPktType3 type3;
+    }data;
+}CAN_MSG;
+#pragma pop
 
 #endif
--- new_acquisition/thread_main.cpp
+++ new_acquisition/thread_main.cpp
@@ -1,13 +1,16 @@
 #include "mbed.h"
 #include "canPayloadCreator.hh"
-#include "sharedWithAcquisitionSW.hh"
  
 #define THREAD_TIME_INTERVAL_SEC    2
+extern bool bLock;
+extern canPayloadCreator * CPC;
 Serial pc(USBTX, USBRX); // tx, rx
 DigitalOut led1(LED1);
 
 Ticker dataTimer;
 bool bTimerFlag = false;
+
+extern void thread_canPayloadCreator_main(void);
 
 void timerCallback(void) {
     bTimerFlag = true;
@@ -16,42 +19,44 @@ void timerCallback(void) {
 void addDataToBuffer(unsigned int sensorIdx, int * pData){
     led1 = !led1;    
 
-	if(CPC != NULL)
-	{
-		// TODO ADD MUTEX TO CPC AND CHECK IF IT'S HERE IF AVAILABLE
-		CPC->updateSensorData( sensorIdx, pData );	
-	}    
+    if(CPC != NULL)
+    {
+        // TODO ADD MUTEX TO CPC AND CHECK IF IT'S HERE IF AVAILABLE
+        if(CPC != NULL){
+            pc.printf("%d\r\n", *pData);
+            CPC->updateSensorData( sensorIdx, pData);  
+        }
+    }    
 }
 
 /* main thread*/
 #define SENSOR_1_ID 1
 int main() {
-	/*INIT storage classes needed for all of the threads*/
+    /*INIT storage classes needed for all of the threads*/
     CPC = new canPayloadCreator();
     
     // initialize sensors and add a storage element to the data buffer
     //test, creating one sensor
     CPC->addSensor(SENSOR_1_ID, sizeof(int));   
     
-    CPC->Init();
-	
     // start CAN message threads
-    thread_CANager.start(thread_canPayloadCreator_main);
+    //thread_CANager.start(thread_canPayloadCreator_main);
 
-	// start payload creator thread
-	thread_CPC.start(thread_canPayloadCreator_main);
-	
-	//  TODO: start data acquisition loops
-	// SAMPLE INTERNAL DATA LOOP
-    dataTimer.attach(&timerCallback, SENSOR_ACQ_SECS);
+    // start payload creator thread
+    Thread thread_CPC;
+    thread_CPC.start(thread_canPayloadCreator_main);
+    
+    //  TODO: start data acquisition loops
+    // SAMPLE INTERNAL DATA LOOP
+    dataTimer.attach(&timerCallback, 0.3);
     while(1)
     {
         if(bTimerFlag == true){
             static int count = 1;
-            while(lock != AVAIL){}
-            lock = LOCKED;
+            while(bLock != false){}
+            bLock = true;
             addDataToBuffer( SENSOR_1_ID , &count );
-            lock = AVAIL;
+            bLock = false;
             count++;
             bTimerFlag = false;    
         }

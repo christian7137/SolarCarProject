d5cffa0bc09c422d1ed977af9bac94c2145a4d35
Christian Young <christian.young@utexas.edu>
2017-10-07 22:04:10 -0500

    Update Influx/Grafana script

--- Display/display.sh
+++ Display/display.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-# install Grafana
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' grafana|grep "install ok installed")
-echo Checking for Grafana: $PKG_OK
+# install apt-transport-https
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' apt-transport-https|grep "install ok installed")
+echo Checking for apt-transport-https: $PKG_OK
 if [ "" == "$PKG_OK" ]; then
-	echo "Grafana is not installed. Installing Grafana . . ."
-	wget https://github.com/fg2it/grafana-on-raspberry/releases/download/v4.4.3/grafana_4.4.3_armhf.deb
-	sudo dpkg -i grafana_4.4.3_armhf.deb
-	sudo apt-get install -f
-	
-	# enable run at boot?
-	echo "Enabling Grafana at Boot . . ."
-	sudo systemctl enable grafana-server
-	sudo systemctl start grafana-server
+	echo "apt-transport-https is not installed. Installing . . ."
+	sudo apt-get install apt-transport-https
 	echo "Done!"
 fi
 
@@ -21,22 +14,46 @@ PKG_OK=$(dpkg-query -W --showformat='${Status}\n' influxdb|grep "install ok inst
 echo Checking for InfluxDB: $PKG_OK
 if [ "" == "$PKG_OK" ]; then
 	echo "InfluxDB is not installed. Installing InfluxDB . . ."
-	wget https://dl.influxdata.com/influxdb/releases/influxdb-1.3.5_linux_armhf.tar.gz
-	tar xvfz influxdb-1.3.5_linux_armhf.tar.gz
-	
-	# configure influxdb and finish installing (Maybe)
-	echo "Readying InfluxDB . . ."
-	cd influxdb-1.3.5_linux_armhf
-	./configure
-	make
-	sudo make install
+	wget -O - https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+	echo "deb https://repos.influxdata.com/debian jessie stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+	sudo apt-get update && sudo apt-get install influxdb
 	
 	# enable run at boot?
 	echo "Enabling InfluxDB at Boot . . ."
-	sudo systemctl enable influx
-	sudo systemctl start influx
+	sudo systemctl daemon-reload
+	sudo systemctl enable influxdb.service
+	sudo systemctl start influxdb.service
 	echo "Done!"
 fi
+
+# install Grafana
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' grafana|grep "install ok installed")
+echo Checking for Grafana: $PKG_OK
+if [ "" == "$PKG_OK" ]; then
+	echo "Grafana is not installed. Installing Grafana . . ."
+	echo "deb https://dl.bintray.com/fg2it/deb jessie main" | sudo tee /etc/apt/sources.list.d/grafana-fg2it.list 
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
+	sudo apt-get update && sudo apt-get install grafana
+	
+	# enable run at boot?
+	echo "Enabling Grafana at Boot . . ."
+	sudo systemctl daemon-reload
+	sudo systemctl enable grafana-server.service
+	sudo systemctl start grafana-server.service
+	echo "Done!"
+fi
+
+influx
+expect ">"
+send CREATE DATABASE my_db
+expect ">"
+send CREATE USER admin WITH PASSWORD 'password123' WITH ALL PRIVILEGES
+expect ">"
+send CREATE USER grafana WITH PASSWORD 'letmeread123'
+expect ">"
+send GRANT READ ON my_db TO grafana
+expect ">"
+send exit
 
 echo "Script is done!"
 # Before being able to run script, need to: chmod +x /path/to/display.sh
